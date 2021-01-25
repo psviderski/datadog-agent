66b8151f8bde9473f6b10715278e888515251348
Pavel Sviderski <me@psviderski.name>
2021-01-25 14:20:54 +1100

    do not report zero CPU stats if unable to get it from cgroup

--- pkg/util/containers/providers/cgroup/cgroup_metrics.go
+++ pkg/util/containers/providers/cgroup/cgroup_metrics.go
@@ -193,7 +193,8 @@ func (c ContainerCgroup) CPU() (*metrics.ContainerCPUStats, error) {
 	f, err := os.Open(statfile)
 	if os.IsNotExist(err) {
 		log.Debugf("Missing cgroup file: %s", statfile)
-		return ret, nil
+		log.Warnf("Unable to get CPU stats from cgroup (missing cgroup file): %+v", c)
+		return nil, fmt.Errorf("unable to get CPU stats from cgroup: %s", err)
 	} else if err != nil {
 		return nil, err
 	}
